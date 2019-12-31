a8e57e079ebcf04151fed067ff1f151088a1afae
George Aoyagi <aoyagigeorge@gmail.com>
2019-12-31 02:13:21 -0800

    first commit

---
+++ solver.py
@@ -0,0 +1,13 @@
+#program  that solves a sudoku puzzle for you
+#bmp: solves a a precoded sudoku puzzle thats stored as a 2D list, prints everything out into terminal
+#bike ver: adds a graphical component 
+#car ver: can take a user inputted sudoku puzzle
+
+#made by George Aoyagi
+#solver goes to evey empty spot, and gives you a valid number
+#if no vlaid number can't be found then it rewinds to the previous guest spot and tries to find another valid number for that spot
+#the process continues and rewinds until puzzle is solved   
+
+
+
+
